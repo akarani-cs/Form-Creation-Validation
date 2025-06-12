58bf55658a17dd36ccef668d07ef3ba7c35fa50d
Alvin karani <alvinkaranimudembei@gmail.com>
2025-06-12 16:54:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 
+document.addEventListener("DOMContentLoaded", function(){
+
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault();//Prevent automatic form submission.
+
+        // Validate Username
+        const usernameInput = document.getElementById('username');
+        const usernameError = document.getElemenrById('usernameError');
+
+
+
+
+    //submit event listener.    
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+//for the content loader.
+});
